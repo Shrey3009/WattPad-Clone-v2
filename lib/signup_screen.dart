348a878607f22ee423bc481622f128566b2c1585
Shrey3009 <ajgaonkarshreyas01@gmail.com>
2022-04-16 10:41:22 +0530

    Modified singup_screen.dart

--- lib/signup_screen.dart
+++ lib/signup_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
 
-
 class SignupScreen extends StatefulWidget {
   static const routeName = '/signup';
   _SignupScreenState createState() => _SignupScreenState();
@@ -10,36 +9,36 @@ class SignupScreen extends StatefulWidget {
 class _SignupScreenState extends State<SignupScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey();
   TextEditingController _passwordController = new TextEditingController();
-  void _submit(){
+  void _submit() {
     if (_formKey.currentState!.validate()) {
-                  // If the form is valid, display a snackbar. In the real world,
-                  // you'd often call a server or save the information in a database.
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('You are successfully registered.')),
-                  );
-                }
-
+      // If the form is valid, display a snackbar. In the real world,
+      // you'd often call a server or save the information in a database.
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('You are successfully registered.')),
+      );
+    }
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Sign Up"), 
-        backgroundColor: Colors.black, // appbar color.
-      foregroundColor: Colors.white,
-        actions: <Widget>[
-          FlatButton(
-              onPressed: () {
-                Navigator.of(context)
-                    .pushReplacementNamed(LoginScreen.routeName);
-              },
-              textColor: Colors.white,
-              child: Row(
-                children: <Widget>[
-                  Text('Log in'),
-                  Icon(Icons.person),
-                ],
-              ))
-        ]),
+        appBar: AppBar(
+            title: Text("Sign Up"),
+            backgroundColor: Colors.black, // appbar color.
+            foregroundColor: Colors.white,
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pushReplacementNamed(LoginScreen.routeName);
+                  },
+                  textColor: Colors.white,
+                  child: Row(
+                    children: <Widget>[
+                      Text('Log in'),
+                      Icon(Icons.person),
+                    ],
+                  ))
+            ]),
         body: Stack(children: <Widget>[
           Container(
             decoration: BoxDecoration(
@@ -53,12 +52,11 @@ class _SignupScreenState extends State<SignupScreen> {
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10),
               side: BorderSide(
-      color: Colors.grey.withOpacity(0.2),
-      width: 1,
-    ),
+                color: Colors.grey.withOpacity(0.2),
+                width: 1,
+              ),
             ),
             color: Colors.orange,
-     
             child: Container(
               height: 300,
               width: 300,
@@ -79,8 +77,9 @@ class _SignupScreenState extends State<SignupScreen> {
                       onSaved: (value) {},
                     ),
                     TextFormField(
-                      
-                      decoration: InputDecoration(labelText: 'Password',),
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                      ),
                       obscureText: true,
                       controller: _passwordController,
                       validator: (value) {
@@ -102,17 +101,14 @@ class _SignupScreenState extends State<SignupScreen> {
                         }
                         return null;
                       },
-                      
                       onSaved: (value) {},
                     ),
-
-                     SizedBox(
+                    SizedBox(
                       width: 5.0,
                       height: 15.0,
                       //Card
                     ),
                     RaisedButton(
-                      
                       child: Text("Submit"),
                       onPressed: () {
                         _submit();
